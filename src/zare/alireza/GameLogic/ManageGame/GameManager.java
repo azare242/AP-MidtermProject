c4cd8a4e20fc8cefa5123b6d4fad4cddb13d4111
Alireza Zare <Azare242@gmail.com>
2021-06-09 00:07:50 +0430

    feat: intro night completed

--- src/zare/alireza/GameLogic/ManageGame/GameManager.java
+++ src/zare/alireza/GameLogic/ManageGame/GameManager.java
@@ -1,7 +1,7 @@
 package zare.alireza.GameLogic.ManageGame;
 
 import zare.alireza.ServerSide.Server;
-
+import zare.alireza.ServerSide.PlayerThreads.PlayerOnServer;
 public class GameManager {
 
     private Game game;
@@ -12,8 +12,21 @@ public class GameManager {
         this.server = server;
     }
 
+    private void sendMassageToMayor(){
+        PlayerOnServer playerOnServer = game.getPlayerThread("Mayor");
+        String massage = "hey mayor wakeup , you must know " + game.getPlayerThread("Physician").getUserName() + " is physician";
+        playerOnServer.receiveMassage(massage);
+    }
+    private void sendMassageToGodFather(){
+        PlayerOnServer playerOnServer = game.getPlayerThread("GodFather");
+        String massage = "hey god father wakeup , you must know " + game.getPlayerThread("DoctorLector").getUserName() + " is doctor lector and " + game.getPlayerThread("SimpleMafia").getUserName()
+                + " is simple mafia";
+        playerOnServer.receiveMassage(massage);
+    }
     public void introNight(){
         server.sendMassageToPlayers("Intro Night Started");
+        sendMassageToGodFather();
+        sendMassageToMayor();
         server.sendMassageToPlayers("Intro Night Ended");
     }
 
