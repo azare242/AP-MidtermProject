a1ac1da4cfdf67260f637e98106d7dc28a3b824d
Alireza Zare <Azare242@gmail.com>
2021-06-13 22:30:22 +0430

    feat: add voting thread

---
+++ src/zare/alireza/ServerSide/PlayerThreads/VotingThread.java
@@ -0,0 +1,46 @@
+package zare.alireza.ServerSide.PlayerThreads;
+
+
+import zare.alireza.ServerSide.Server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class VotingThread extends Thread{
+
+    private Server server;
+    private DataOutputStream sender;
+    private DataInputStream receiver;
+    private PlayerOnServer thisPlayer;
+
+    public VotingThread(PlayerOnServer player, Server server, DataOutputStream dataOutputStream, DataInputStream dataInputStream){
+        this.server = server;
+        sender = dataOutputStream;
+        receiver = dataInputStream;
+        thisPlayer = player;
+    }
+
+    @Override
+    public void run(){
+        String vote = "";
+        try {
+            sender.writeUTF("voting");
+            server.getAlivePlayersListToAPlayer(thisPlayer);
+
+            String chose = receiver.readUTF();
+
+            int index = Integer.parseInt(chose);
+            if (index == 0) server.setVote("NoOne");
+            else {
+                vote = server.getPlayerUserName(index);
+                server.setVote(vote);
+            }
+            if (server.allPlayersVoted()){
+                server.checkVotes();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
