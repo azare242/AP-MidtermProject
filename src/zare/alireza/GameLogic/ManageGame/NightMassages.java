b25edee8c57187ccfb1bface712816e21e8fc38f
Alireza Zare <Azare242@gmail.com>
2021-06-14 15:45:48 +0430

    feat: night Massages Class with static Strings added

---
+++ src/zare/alireza/GameLogic/ManageGame/NightMassages.java
@@ -0,0 +1,24 @@
+package zare.alireza.GameLogic.ManageGame;
+
+public class NightMassages {
+
+    public static final String eveyOneSleep = "EVERY ONE SLEEP";
+    public static final String mafiaWakeUp = "MAFIA WAKEUP";
+    public static final String godFatherTarget = "GODFATHER SHOW ME HIS TARGET";
+    public static final String mafiaSleep = "MAFIA SLEEP";
+    public static final String doctorLectorWakeUp = "DOCTOR LECTOR WAKEUP";
+    public static final String doctorLectorSleep = "DOCTOR LECTOR SLEEP";
+    public static final String physicianWakeUp = "PHYSICIAN WAKE UP";
+    public static final String physicianSleep = "PHYSICIAN SLEEP";
+    public static final String detectorWakeUp = "DETECTOR WAKE UP";
+    public static final String detectorSleep = "DETECTOR SLEEP";
+    public static final String proWakeUp = "PROFESSIONAL WAKE UP";
+    public static final String proSleep = "PROFESSIONAL SLEEP";
+    public static final String psychologistWakeUp = "PSYCHOLOGIST WAKE UP";
+    public static final String psychologistSleep = "PSYCHOLOGIST SLEEP";
+    public static final String ironSideWakeUp = "IRON SIDE WAKE UP";
+    public static final String ironSideSleep = "IRON SIDE SLEEP";
+    public static final String everyOneWakeUp = "EVERY ONE WAKEUP";
+
+
+}
